9548e322f4ec3bce71dca18871ff258d997065db
s-taku0502 <takumi.daimonhigh@gmail.com>
2026-03-07 19:10:52 +0900

    簡易的なヘッダーコンポーネントの実装

--- frontend/app/components/header.tsx
+++ frontend/app/components/header.tsx
@@ -8,6 +8,7 @@ export default function Header() {
             <nav className="space-x-4">
                 <a href="#features" className="text-blue-700 hover:underline">特徴</a>
                 <a href="#fish" className="text-blue-700 hover:underline">魚図鑑</a>
+                <a href="#recommended_fish_type" className="text-blue-700 hover:underline">おすすめの魚種</a>
                 <a href="#contact" className="text-blue-700 hover:underline">お問い合わせ</a>
             </nav>
         </header>
